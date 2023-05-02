6693bf0d9b9def51ea0784f0caae40175323fe70
Rob <112796163+RobCovasa@users.noreply.github.com>
2023-05-02 09:37:28 +0200

    Added main functions

---
+++ main.py
@@ -0,0 +1,62 @@
+import fire
+from datetime import datetime
+from habit_manager import create_habit, edit_habit, delete_habit, get_habit_by_name
+from analytics import streak_calc, habits_filter, generate_summary_report
+from data_storage import load_info, save_info
+
+class HabitTrackerCLI:
+    def __init__(self, file_path="habits.json"):
+        self.file_path = file_path
+        self.habit_list = load_info(file_path)
+
+    def create(self, name, description, start_date, periodicity):
+        '''Create a new habit with the given name, description, start date, and periodicity.'''
+        habit = create_habit(name, description, datetime.fromisoformat(start_date), periodicity)
+        self.habit_list.append(habit)
+        save_info(self.habit_list, self.file_path)
+        print(f"Habit '{name}' created successfully")
+
+    def edit(self, habit_name, name=None, description=None, start_date=None, periodicity=None):
+        '''Edit a habit with the given name, description, start date, and periodicity.'''
+        habit = get_habit_by_name(self.habit_list, habit_name)
+        if habit:
+            if start_date:
+                start_date = datetime.fromisoformat(start_date)
+            edit_habit(habit, name=name, description=description, start_date=start_date, periodicity=periodicity)
+            save_info(self.habit_list, self.file_path)
+            print(f"Habit '{habit_name}' edited successfully")
+        else:
+            print(f"Habit '{habit_name}' not found")
+
+    def delete(self, habit_name):
+        '''Delete a habit with the given name.'''
+        habit = get_habit_by_name(self.habit_list, habit_name)
+        if habit:
+            delete_habit(self.habit_list, habit)
+            save_info(self.habit_list, self.file_path)
+            print(f"Habit '{habit_name}' deleted successfully")
+        else:
+            print(f"Habit '{habit_name}' not found")
+
+    def streak(self, habit_name):
+        '''Get the current streak for a habit with the given name.'''
+        habit = get_habit_by_name(self.habit_list, habit_name)
+        if habit:
+            print(f"Current streak for habit '{habit_name}': {streak_calc(habit)}")
+        else:
+            print(f"Habit '{habit_name}' not found")
+
+    def filter(self, periodicity):
+        '''Filter habits by periodicity and print them.'''
+        filtered_habits = habits_filter(self.habit_list, periodicity)
+        for habit in filtered_habits:
+            print(f"{habit.name} ({habit.description})")
+
+    def report(self):
+        '''Generate a summary report of all habits and print it.'''
+        report = generate_summary_report(self.habit_list)
+        for habit_report in report:
+            print(f"{habit_report['habit_name']} - Streak: {habit_report['streak']} - Total Completions: {habit_report['total_completions']}")
+
+if __name__ == "__main__":
+    fire.Fire(HabitTrackerCLI)
