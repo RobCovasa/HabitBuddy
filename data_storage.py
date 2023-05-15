753d0512248da82d98b3bcace0c7c35908f9dc14
Rob <112796163+RobCovasa@users.noreply.github.com>
2023-05-15 09:35:49 +0200

    Updated comments and docstring

--- data_storage.py
+++ data_storage.py
@@ -3,27 +3,32 @@ from habit_manager import Habit
 from colorama import Fore, Style
 
 def load_info(file_path):
-    '''Load data from a JSON file and return a list of habits'''
+    '''Load data from a JSON file'''
     print(f"{Fore.YELLOW}{Style.BRIGHT}Loading data from file: {file_path}{Style.RESET_ALL}")
-    # try and except to catch errors
+    # Catch and handle any errors that may occur during file operations
     try:
         with open(file_path, "r") as file:
             data = json.load(file)
+            # Convert each dictionary in data["habits"] into a Habit object using Habit.from_dictionary
             habit_list = [Habit.from_dictionary(habit_dict) for habit_dict in data["habits"]]
             print(f"{Fore.GREEN}Data loaded successfully{Style.RESET_ALL}")
             return habit_list
-    except (FileNotFoundError, json.JSONDecodeError): # FileNotFoundError to catch if file doesn't exist, JSONDecodeError to catch if file is empty
+    # Catch FileNotFoundError if the file does not exist and JSONDecodeError if the file content is not valid JSON
+    except (FileNotFoundError, json.JSONDecodeError): 
         print(f"{Fore.RED}Error: {Style.RESET_ALL} File not found or empty")
-        return []
+        return [] # Return an empty list if an error occurred
 
 def save_info(habit_list, file_path):
     '''Save data to a JSON file'''
     print(f"{Fore.GREEN}Saving data to file: {file_path}{Style.RESET_ALL}")
+    # Convert each Habit object in habit_list to a dictionary using Habit.to_dictionary
     data = {"habits": [habit.to_dictionary() for habit in habit_list]}
-    # try and except to catch errors
+    # Catch and handle any errors that may occur during file operations
     try:
         with open(file_path, "w") as file:
-            json.dump(data, file, indent=4) # Indent to make the file more readable
+            # Use json.dump to write data to the file, indenting it for readability
+            json.dump(data, file, indent=4) 
             print(f"{Fore.GREEN}Data saved successfully{Style.RESET_ALL}")
-    except IOError: # IOError to catch if file is read-only
+    # Catch IOError
+    except IOError: 
         print(f"{Fore.RED}Error: {Style.RESET_ALL} Saving data failed")
